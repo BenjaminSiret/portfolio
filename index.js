c57b1d088f80bd156422534db0aae517e483e082
Benjamin Siret <benjaminsiret@outlook.fr>
2025-02-12 17:03:18 +0100

    last changes for project cards, animations, clean css and js

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Initialisation des éléments DOM
   const menuToggle = document.querySelector("#menu-toggle");
   const menuMobile = document.querySelector(".menu-mobile");
   const overlay = document.querySelector(".overlay");
   const menuMobileItems = document.querySelectorAll(".menu-mobile-item");
   const body = document.body;
+  const modal = document.getElementById("project-modal");
+  const closeModalButton = document.querySelector(".close-button");
+  const modalDescription = document.querySelector(".modal-description");
+  const modalRepoLink = document.querySelector(".repo-link");
+  const modalImage = document.querySelector(".modal-image");
+  const projectArticles = document.querySelectorAll(".projects-cards article");
+  const repoLink = modal.querySelector(".modal-content a");
+  const focusableElements = [closeModalButton, repoLink];
+  let lastFocusedElement;
 
   const toggleMenu = () => {
     const isExpanded = menuMobile.classList.contains("menu-mobile-open");
-
     menuMobile.classList.toggle("menu-mobile-open");
     overlay.classList.toggle("overlay-open");
     body.classList.toggle("body-no-scroll");
@@ -20,20 +29,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  menuToggle.addEventListener("click", toggleMenu);
-  overlay.addEventListener("click", toggleMenu);
+  const handleModalClose = () => {
+    modal.classList.remove("modal-open");
+    setTimeout(() => {
+      modal.style.display = "none";
+      body.classList.remove("body-no-scroll");
+      if (lastFocusedElement) lastFocusedElement.focus();
+    }, 400);
+  };
 
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape" && menuMobile.classList.contains("menu-mobile-open")) {
-      toggleMenu();
-    }
-  });
+  const openModal = (article) => {
+    lastFocusedElement = document.activeElement;
+    modal.style.display = "flex";
+    setTimeout(() => {
+      modal.classList.add("modal-open");
+    }, 10); // Petit délai pour permettre l'application de la classe
+    modalImage.src = article.querySelector("img").src;
+    modalDescription.textContent = article.dataset.description;
+    modalRepoLink.href = article.dataset.url;
+    body.classList.add("body-no-scroll");
+    closeModalButton.focus();
+  };
 
-  //**************************************************************************
-  // Gestion des flèches du clavier
-  //**************************************************************************
-
-  menuMobile.addEventListener("keydown", (e) => {
+  const handleMenuKeyboardNavigation = (e) => {
     const currentIndex = Array.from(menuMobileItems).indexOf(document.activeElement);
     let nextIndex;
 
@@ -66,26 +84,36 @@ document.addEventListener("DOMContentLoaded", () => {
       default:
         return;
     }
-  });
+  };
 
-  menuMobileItems.forEach((item) => {
-    item.addEventListener("click", toggleMenu);
-  });
+  const handleModalKeyboardNavigation = (e) => {
+    if (e.key === "Tab") {
+      const firstFocusable = focusableElements[0];
+      const lastFocusable = focusableElements[focusableElements.length - 1];
 
-  //**************************************************************************
-  // Observers pour les entrées de texte
-  //**************************************************************************
+      if (e.shiftKey) {
+        if (document.activeElement === firstFocusable) {
+          e.preventDefault();
+          lastFocusable.focus();
+        }
+      } else {
+        if (document.activeElement === lastFocusable) {
+          e.preventDefault();
+          firstFocusable.focus();
+        }
+      }
+    }
+  };
 
   const observerElement = (selector, animationClass, delay) => {
-    const element = document.querySelector(selector);
-    if (!element) return;
+    const elements = document.querySelectorAll(selector);
+    if (elements.length <= 0) return;
 
     const observer = new IntersectionObserver(
-      (entries, observer) => {
+      (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            const element = entry.target;
-            element.classList.add(animationClass);
+            entry.target.classList.add(animationClass);
             observer.unobserve(entry.target);
           }
         });
@@ -93,26 +121,80 @@ document.addEventListener("DOMContentLoaded", () => {
       { threshold: delay }
     );
 
-    observer.observe(element);
+    elements.forEach((element) => {
+      observer.observe(element);
+    });
   };
 
+  // Initialisation des événements
+  menuToggle.addEventListener("click", toggleMenu);
+  overlay.addEventListener("click", toggleMenu);
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" && menuMobile.classList.contains("menu-mobile-open")) {
+      toggleMenu();
+    }
+  });
+
+  menuMobile.addEventListener("keydown", handleMenuKeyboardNavigation);
+  menuMobileItems.forEach((item) => {
+    item.addEventListener("click", toggleMenu);
+  });
+
+  const backgroundDescriptionClass = window.innerWidth <= 768 ? "fade-in-right" : "fade-in-bottom";
   observerElement(".about-title", "fade-in-right", 0.5);
   observerElement(".about-description", "fade-in-right-delay-1", 0.5);
-  observerElement(".background-description", "fade-in-bottom", 0.5);
+  observerElement(".background-description", backgroundDescriptionClass, 0.5);
+  observerElement(".projects-cards article", "fade-in-bottom", 0.5);
 
-  //**************************************************************************
-  // Animation du download__link
-  //**************************************************************************
+  const downloadLink = document.querySelector(".download-link");
+  const contactLinks = document.querySelectorAll(".contact-info a");
 
-  const link = document.querySelector(".download-link");
-
-  link.addEventListener("mouseenter", () => {
+  const handleLinkAnimation = (link) => {
     link.classList.add("spin-animation");
     link.classList.remove("spin-exit");
-  });
+  };
 
-  link.addEventListener("mouseleave", () => {
+  const handleLinkAnimationExit = (link) => {
     link.classList.remove("spin-animation");
     link.classList.add("spin-exit");
+  };
+
+  [downloadLink, ...contactLinks].forEach((link) => {
+    link.addEventListener("mouseenter", () => handleLinkAnimation(link));
+    link.addEventListener("mouseleave", () => handleLinkAnimationExit(link));
   });
+
+  projectArticles.forEach((article) => {
+    article.addEventListener("click", (e) => {
+      e.preventDefault();
+      openModal(article);
+    });
+    article.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        e.preventDefault();
+        openModal(article);
+      }
+    });
+  });
+
+  closeModalButton.addEventListener("click", handleModalClose);
+  closeModalButton.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      handleModalClose();
+    }
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      handleModalClose();
+    }
+  });
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" && modal.style.display === "flex") {
+      handleModalClose();
+    }
+  });
+
+  modal.addEventListener("keydown", handleModalKeyboardNavigation);
 });
